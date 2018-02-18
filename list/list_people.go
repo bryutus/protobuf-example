5dc8e3897cdd292f5ca43c78c56901f97f82ca4a
bryutus <bryutus@gmail.com>
2018-02-18 18:08:59 +0900

    :sparkles: unmarshal addressbook

--- list/list_people.go
+++ list/list_people.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/golang/protobuf/proto"
+	pb "github.com/bryutus/protobuf-tutorial/tutorial"
+)
+
+// Main reads the entire address book from a file and prints all the
+// information inside.
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s ADDRESS_BOOK_FILE\n", os.Args[0])
+	}
+	fname := os.Args[1]
+
+	// [START unmarshal_proto]
+	// Read the existing address book.
+	in, err := ioutil.ReadFile(fname)
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+
+	book := &pb.AddressBook{}
+	if err := proto.Unmarshal(in, book); err != nil {
+		log.Fatalln("Failed to parse address book:", err)
+	}
+	// [END unmarshal_proto]
+}
