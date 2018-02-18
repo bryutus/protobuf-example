64ccb5cb241e7a086f2f19398889b58bea75963b
bryutus <bryutus@gmail.com>
2018-02-18 17:05:39 +0900

    :tada: output file with input

--- add_person.go
+++ add_person.go
@@ -122,4 +122,15 @@ func main() {
 	}
 	book.People = append(book.People, addr)
 	// [End_EXCUTE]
+
+	// Write the new address book back to disk.
+	out, err := proto.Marshal(book)
+	if err != nil {
+		log.Fatalln("Failed to encode address book:", err)
+	}
+
+	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Failed to write address book:", err)
+	}
+	// [End marshal_proto]
 }
